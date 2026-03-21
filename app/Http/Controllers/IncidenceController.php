cc5027520a7c27239a0195685da8dd7a18c09ffa
Edwin Ontaneda Ríos <marcontanedarg@gmail.com>
2026-03-21 14:49:59 +0100

    Creación de controller: IncidenceController

---
+++ app/Http/Controllers/IncidenceController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Incidence;
+use App\Models\Resource;
+
+class IncidenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'resource_id' => 'required|exists:resources, resource_id',
+            'description' => 'required|string',
+        ]);
+
+        //Create incidence
+        Incidence::create([
+            'resource_id' => $request->resource_id,
+            'user_id' => auth()-> id(),
+            'description' => $request->description,
+            'date_incidence' => now(),
+            'status' => false,
+            'created_by' => auth()->id(),
+        ]);
+
+        $resource = Resource::find($request->resource_id);
+        $resource->update(['status' => 2]);
+
+        return redirect()->back()->with('info', 'Incidencia reportada');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
