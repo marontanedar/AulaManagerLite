0d4732b17fb7d580adf814b449029d32817d3de6
Edwin Ontaneda Ríos <marcontanedarg@gmail.com>
2026-03-21 14:47:16 +0100

    Routte para pruebas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\IncidenceController;
+use App\Http\Controllers\ResourceController;
+use App\Models\Reservation;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/reservations', function () {
+    return "Construction";
+})->name('reservation.index');
+
+//Route::middleware(['auth'])->group(function () {
+    Route::resource('resources', ResourceController::class);
+    Route::resource('incidences', IncidenceController::class);
+//});
