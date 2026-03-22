6c9bef3c11512bd113edbf545f511462ff3b2ff4
Edwin Ontaneda Ríos <marcontanedarg@gmail.com>
2026-03-22 15:04:26 +0100

    Modificación para guest y nuevas rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\IncidenceController;
 use App\Http\Controllers\ResourceController;
+use App\Http\Controllers\AuthController;
 use App\Models\Reservation;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
@@ -21,11 +22,23 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/reservations', function () {
-    return "Construction";
-})->name('reservation.index');
+// Route::get('/reservations', function () {
+//     return "Construction";
+// })->name('reservation.index');
 
-//Route::middleware(['auth'])->group(function () {
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name(('login'));
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
     Route::resource('resources', ResourceController::class);
     Route::resource('incidences', IncidenceController::class);
-//});
+
+    Route::get('/reservations', function () {
+        return "Reservas";
+    })->name('reservations.index');
+});
