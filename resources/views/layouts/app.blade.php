4a465c69fbd3861819160c847fcba0af89894182
Edwin Ontaneda Ríos <marcontanedarg@gmail.com>
2026-03-22 18:45:50 +0100

    corrección reservations, incidences

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,13 +27,14 @@
         <div class="collapse navbar-collapse">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              
+              <a class="nav-link" href="{{ route('reservations.index') }}">Reservas</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="{{ route('resources.index') }}">Recursos</a>
             </li>
             <li class="nav-item">
-              
+              <a class="nav-link" href="{{ route('incidences.index') }}">Incidencias</a>
+            </li>
             </li>
           </ul>
         </div>
