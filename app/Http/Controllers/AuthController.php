b0b7e99872727faaac2385ad1b1ea12c953cdbdc
Edwin Ontaneda Ríos <marcontanedarg@gmail.com>
2026-03-25 13:24:36 +0100

    Cambio en ruta de función login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('reservations.index');
+            return redirect()->intended(route('reservations.index'));
         }
 
         return back()->withErrors([
