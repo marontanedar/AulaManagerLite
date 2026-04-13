53a2c39d9ed85cf97ab6fb333292474232170ee4
Edwin Ontaneda Ríos <marcontanedarg@gmail.com>
2026-04-13 16:20:53 +0200

    Añadido nuevo seeder spaces

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             CategorySeeder::class,
+            SpaceSeeder::class,
             ResourceSeeder::class,
         ]);
     }
